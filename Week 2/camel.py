ff8cf170208b9e102f972db4b992bb9cff8d4491
ReyhanehEstiri <108826791+ReyhanehEstiri@users.noreply.github.com>
2023-07-30 17:50:35 +0330

    Create camel.py

---
+++ Week 2/camel.py
@@ -0,0 +1,10 @@
+Input = input("camelCase: ")
+print("snake_case: ", end="")
+
+for l in Input:
+    if l.isupper():
+        print("_" + l.lower(), end="")
+    else:
+        print(l , end="")
+
+print()
