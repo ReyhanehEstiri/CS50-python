9d1c20ab2775bcf17ce8a720479c0688869b6cd5
ReyhanehEstiri <108826791+ReyhanehEstiri@users.noreply.github.com>
2023-08-08 20:40:07 +0330

    Create game.py

---
+++ Week 4/game.py
@@ -0,0 +1,24 @@
+import random
+def main():
+    level = 0
+    while True:
+        try:
+            level = int(input("Level: "))
+            x = random.randint(1, level)
+            while True:
+                guess = int(input("Guess: "))
+                if guess > x:
+                    print("Too large!")
+                elif guess < x:
+                    print("Too small!")
+                elif guess == x:
+                    print("Just right!")
+                    raise EOFError
+        except ValueError:
+            pass
+        except EOFError:
+            break
+
+
+if __name__ == "__main__":
+    main()
