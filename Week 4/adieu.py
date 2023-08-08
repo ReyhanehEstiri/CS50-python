4a0dcb6611ec729214c310d3f0f149df20e74ee8
ReyhanehEstiri <108826791+ReyhanehEstiri@users.noreply.github.com>
2023-08-08 20:40:35 +0330

    Create adieu.py

---
+++ Week 4/adieu.py
@@ -0,0 +1,12 @@
+import inflect
+p = inflect.engine()
+
+names = []
+
+while True:
+    try:
+        name = input("Name: ").strip()
+        names.append(name)
+    except EOFError:
+        print("Adieu, adieu, to", p.join(names))
+        break
