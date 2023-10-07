1b9172fd0a7a9c2f76792508bc1621452088e355
Reyhaneh <108826791+ReyhanehEstiri@users.noreply.github.com>
2023-10-07 08:07:44 +0330

    Add files via upload

---
+++ Week 7/um/um.py
@@ -0,0 +1,11 @@
+import re
+
+def main():
+    print(count(input("Text: ")))
+
+def count(s):
+    count=re.findall(r'\bum\b', s, re.IGNORECASE)
+    return len(count)
+
+if __name__ == "__main__":
+    main()
